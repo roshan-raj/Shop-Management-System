72ac95c3774682c3f081deebf6677213239ee7e0
Roshan Raj <roshan139154@gmail.com>
2020-11-02 10:24:20 +0530

    validation middleware

--- server/middleware/validateMiddleware.js
+++ server/middleware/validateMiddleware.js
@@ -1,2 +1,19 @@
 // This middleware is used to check if the express-validator middleware returns an error.
 // If so, it recreates the error object using the param and msg keys and returns the error.
+const { validationResult } = require('express-validator');
+const { returnResponse, formatResponse } = require("./responseMiddleware");
+
+let validate = (req, res, next) => {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        let error = {};
+        errors.array().map((err) => error[err.param] = err.msg);
+        return returnResponse(res, formatResponse(400, null, error));
+    }
+    next();
+};
+
+module.exports = {
+    validate
+}
